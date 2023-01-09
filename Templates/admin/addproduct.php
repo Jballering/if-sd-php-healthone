d55720b4fd399b886741c91a15f2159ba3f5c628
Jballering <302759602@student.rocmondriaan.nl>
2023-01-09 14:31:38 +0100

    db mee genomen en add afgemaakt
    a

--- Templates/admin/addproduct.php
+++ Templates/admin/addproduct.php
@@ -13,10 +13,38 @@ include ("../defaults/head.php");
     ?>
     <?php if (!empty($message)): ?>
     <div class="alert alert-success" role="alert">
-        <?$=$message?>
+        <?=$message?>
     </div>
     <?php endif;?>
-    <form class="row g-3" method="post" enctype="multipart/form-data"></form>
+    <form class="row g-3" method="post" enctype="multipart/form-data">
+    <div class="col-md-6">
+        <label for="inputName" class="form-label">naam</label>
+    <input type="text" class="form-control" id="inputName" name="name" value="<? if (isset($_POST['name'])) {echo $_POST['name'];}
+    else {
+        echo ''; }?>">
+    </div>
+        <div class="col-md-6"
+             <label for="cat" class="form-label">Category</label>
+        <select class="form-select" id="cat" name="category">
+        <?php
+        foreach ($categories as $category):
+        ?>
+            <option value="<?=$category->id?>"><?=$category->name?></option>
+            <?php endforeach;?>
+        </select>
+        </div>
+        <div class="col-12">
+            <label for="formFile" class="form-label">Image bij sportapparaat</label>
+            <input class="form-control" name="fileToUpload" type="file" id="formFile">
+        </div>
+
+        <div class="col-12">
+            <button type="submit" name="submit" class="btn btn-primary">add</button>
+        </div>
+    </form>
+<?php
+include_once ('../defaults/footer.php');
+?>
 </div>
 </body>
 </html>
---
+++ database/healthone.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 09 jan 2023 om 14:29
+-- Serverversie: 10.4.24-MariaDB
+-- PHP-versie: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `healthone`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'kanos'),
+(2, 'fietsen');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `fietsen`
+--
+
+CREATE TABLE `fietsen` (
+  `id` int(11) NOT NULL,
+  `naam` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `prijs` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `fietsen`
+--
+ALTER TABLE `fietsen`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT voor een tabel `fietsen`
+--
+ALTER TABLE `fietsen`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
